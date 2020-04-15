b36df81591b9e7b1009a0eb97b7cee2c79c51c30
AC <Acevedooh94@gmail.com>
2020-04-14 22:01:08 -0400

    first commit - arranca desde aqui

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+///initializations
+
+const app = express();
+
+//settings
+
+app.set('port', process.env.app || 4000);
+
+
+// Middlewares : son funciones que se ejecuta cuando un usuario envia una peticion 
+
+app.use(morgan('dev'));
+
+// Global Variables
+
+// Routes
+
+
+// Public
+
+
+// Starting the server
+
+app.listen(app.get('port'), () => {
+
+   console.log('server on port', app.get('port'));
+
+});
